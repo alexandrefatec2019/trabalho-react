728dc410700a43ad532cd5042f00b8dfe2cc2ce7
ALEXANDRE YAMASAKI <alexandre.yamasaki@fatec.sp.gov.br>
2021-11-18 22:56:14 -0300

    asfd

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
+import { useState } from 'react'
 import './App.css';
 import Adicionar from './Components/Adicionar';
 import Listar from './Components/Listar';
+import { createContext } from 'react';
+
+export const Context = createContext({});
 
 function App() {
+
+  const [dados, setDados] = useState(Blog);
+
   return (
     <div className="App">
       <header className="App-header">
-        <Adicionar />
-        <Listar />
+        <Context.Provider value={[dados, setDados]}>
+          <Adicionar />
+          <Listar />
+        </Context.Provider>
       </header>
     </div>
   );
 }
 
 export default App;
+
+export class Blog{
+  constructor(id,name) {
+    this.id = id;
+    this.name = name;
+  }
+}
--- src/Components/Listar/index.js
+++ src/Components/Listar/index.js
@@ -1,9 +1,16 @@
-export default function Listar(){
-    return(
+import { useContext } from 'react'
+import { Context } from '../../App'
+
+export default function Listar() {
+
+    const [dados, setDados] = useContext(Context);
+
+
+    return (
         <div className="card">
-        <div className="card-body">
-            
+            <div className="card-body">
+                {dados}
+            </div>
         </div>
-    </div>
     )
 }
