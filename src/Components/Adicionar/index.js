f39e00534637c1c8a623d6ba3225a2fa0dbdba5a
ALEXANDRE YAMASAKI <alexandre.yamasaki@fatec.sp.gov.br>
2021-11-18 22:55:50 -0300

    asdasfa

--- src/Components/Adicionar/index.js
+++ src/Components/Adicionar/index.js
@@ -1,8 +1,37 @@
-export default function Adicionar(){
-    return(
+import { useContext } from 'react'
+import { Context, Blog } from '../../App'
+
+export default function Adicionar() {
+
+
+
+    const [dados, setDados] = useContext(Context);
+
+    function HandleSubmit(e) {
+        e.preventDefault();
+
+        const data = new FormData(e.target);
+        setDados(...dados, [data]);
+        Blog.
+        console.log(data.get('titulo'));
+
+
+    }
+
+    return (
         <div className="card">
             <div className="card-body">
-                
+                <form onSubmit={HandleSubmit}>
+                    <div className="mb-0">
+                        <label className="form-label">Título</label>
+                        <input name="titulo" id="titulo" type="text" className="form-control" required />
+                    </div>
+                    <div className="mb-0">
+                        <label className="form-label">Texto</label>
+                        <input name="texto" type="text" className="form-control" required />
+                    </div>
+                    <button type="submit" className="btn btn-primary btn-sm">Adicionar</button>
+                </form>
             </div>
         </div>
     )
